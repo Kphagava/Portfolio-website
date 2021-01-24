f3cac8634677985eb544c55e6a0e2652fc4b4d94
Lasha Loladze <llola16@freeuni.edu.ge>
2021-01-24 14:58:19 +0400

    Added progressbar

---
+++ app.js
@@ -0,0 +1,24 @@
+const circle = document.querySelector('.progress-ring__circle');
+const radius = circle.r.baseVal.value;
+const circumference = radius * 2 * Math.PI;
+
+circle.style.strokeDasharray = `${circumference} ${circumference}`;
+circle.style.strokeDashoffset = circumference;
+
+function setProgress(percent) {
+    const offset = circumference - percent / 100 * circumference;
+    circle.style.strokeDashoffset = offset;
+}
+
+
+document.addEventListener('scroll', function(e) {
+    let scrollHeight = Math.max(
+        document.body.scrollHeight, document.documentElement.scrollHeight,
+        document.body.offsetHeight, document.documentElement.offsetHeight,
+        document.body.clientHeight, document.documentElement.clientHeight
+    );
+    let last_known_scroll_position = window.scrollY;
+    let max = scrollHeight - document.documentElement.clientHeight;
+    let percentage = (last_known_scroll_position/max)*100;
+    setProgress(percentage);
+});
