8364160169e6ab0a33c7af105cc49aa34b8fbbad
Lasha Loladze <llola16@freeuni.edu.ge>
2021-01-30 14:10:10 +0400

    Added progress ring

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const circle = document.querySelector('.progress-ring__circle');
+const ring = document.querySelector('.progress-ring');
 const radius = circle.r.baseVal.value;
 const circumference = radius * 2 * Math.PI;
 
@@ -10,6 +11,10 @@ function setProgress(percent) {
     circle.style.strokeDashoffset = offset;
 }
 
+circle.addEventListener('click', function(e) {
+    ring.classList.remove('active');
+    window.scrollTo({top: 0, behavior: 'smooth'});
+})
 
 document.addEventListener('scroll', function(e) {
     let scrollHeight = Math.max(
@@ -20,5 +25,10 @@ document.addEventListener('scroll', function(e) {
     let last_known_scroll_position = window.scrollY;
     let max = scrollHeight - document.documentElement.clientHeight;
     let percentage = (last_known_scroll_position/max)*100;
+    
+    const isAtStart = percentage === 0; 
+    if(!isAtStart) ring.classList.add('active');
+    else ring.classList.remove('active');
+    
     setProgress(percentage);
 });
