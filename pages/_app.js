4e9591328723dea862b50a0a079f503e21e46f7c
Kphagava <k.phagava@gmail.com>
2021-02-12 20:23:32 +0400

    Added basic pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import Layout from '../components/layout'
+import Link from 'next/link'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <Link href="/work">
+        <a>Work</a>
+      </Link>
+      <Link href="/certificates">
+        <a>Certificates</a>
+      </Link>
+      <Link href="/articles">
+        <a>Articles</a>
+      </Link>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
