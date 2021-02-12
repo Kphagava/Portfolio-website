62ad808b6f5ac186507e2abcf662ab7db87ee7a8
Lasha Loladze <llola16@freeuni.edu.ge>
2021-02-12 22:43:33 +0400

    Added navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,12 @@
 import '../styles/globals.css'
 import Layout from '../components/layout'
-import Link from 'next/link'
+import Navbar from '../components/navbar'
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-      <Link href="/">
-        <a>Home</a>
-      </Link>
-      <Link href="/work">
-        <a>Work</a>
-      </Link>
-      <Link href="/certificates">
-        <a>Certificates</a>
-      </Link>
-      <Link href="/articles">
-        <a>Articles</a>
-      </Link>
+      <Navbar/>
       <Component {...pageProps} />
     </Layout>
   )
