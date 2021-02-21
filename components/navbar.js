b82c24e91378640b428c7acff15cecd8c6d28364
Lasha Loladze <lashaloladze@Lashas-MacBook-Pro.local>
2021-02-21 11:28:14 +0400

    Added fonts/header

--- components/navbar.js
+++ components/navbar.js
@@ -1,22 +1,28 @@
+import { useState } from 'react'
 import Link from 'next/link'
+
 import styles from './navbar.module.css'
 
 
 const Navbar = () => {
 
-    let burgerMenuActive = false;
+    const [burgerMenuState, setBurgerMenuState] = useState(false);
 
-    function changeMenuState(){
-        burgerMenuActive = !burgerMenuActive;
-        console.log("ss")
+    const toggleMenu = (e) => {
+        e.preventDefault()  
+        setBurgerMenuState(!burgerMenuState)
     }
+
     return (
-        <header>
+        <header className={styles.header}>
             <div className={styles.breadcrumb}>
                 
             </div>
 
-            <button onClick={() => changeMenuState()} className={[styles.burgerMenu, styles.active].join(" ")}>
+            <button 
+                onClick={toggleMenu} 
+                className={ burgerMenuState ? [styles.burgerMenu, styles.active].join(' ') : styles.burgerMenu}
+            >
                 <svg viewBox="0 0 64 48">
                     <path d="M19,15 L45,15 C70,15 58,-2 49.0177126,7 L19,37"></path>
                     <path d="M19,24 L45,24 C61.2371586,24 57,49 41,33 L32,24"></path>
@@ -24,19 +30,21 @@ const Navbar = () => {
                 </svg>
             </button>
 
-            <Link href="/">
-                <a>Home</a>
-            </Link>
-            <Link href="/work">
-                <a>Work</a>
-            </Link>
-            <Link href="/certificates">
-                <a>Certificates</a>
-            </Link>
-            <Link href="/articles">
-                <a>Articles</a>
-            </Link>
-            <div className={styles.sidenavOverlay}></div>
+            <nav className={ burgerMenuState ? [styles.sidenav, styles.nav, styles.active].join(' ') : [styles.sidenav, styles.nav].join(' ')}>
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+                <Link href="/work">
+                    <a>Work</a>
+                </Link>
+                <Link href="/certificates">
+                    <a>Certificates</a>
+                </Link>
+                <Link href="/articles">
+                    <a>Articles</a>
+                </Link>
+            </nav>
+            <div className={ burgerMenuState ? [styles.sidenavOverlay, styles.active].join(' '): styles.sidenavOverlay}></div>
         </header>
     )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import Layout from '../components/layout'
 import Navbar from '../components/navbar'
 
-
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
