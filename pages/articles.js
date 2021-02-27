53f19b986855f923c9eac6b758d8ab7e7792b007
Lasha Loladze <l.loladze11@gmail.com>
2021-02-27 22:14:58 +0400

    Added articles and work data

--- pages/articles.js
+++ pages/articles.js
@@ -1,7 +1,33 @@
-export default function Articles(){
+const Articles = () => {
+
+    const articles = [
+        {
+            name: 'Challenges of doing UX design in online gambling',
+            desc: 'So I recently quit my job in one of the leading online gambling companies in Georgia, Tbilisi. So I decided to sum up the challenges and…',
+            imgUrl: 'gambling.png'
+        },
+        {
+            name: 'Recruitment platform — JOBSPACE',
+            desc: 'Jobspace is my first attempt to create a startup. Me and my partner started working on it as a fun side project, but continued pursuing…',
+            imgUrl: 'jobspace.png'
+        },
+        {
+            name: 'The usability of road signs',
+            desc: 'The history of road signs goes back to the early 1800s and the first road signs were meant for bicycles, they warned cyclists about bad roads, dangerous turns, etc.',
+            imgUrl: 'roadSigns.png'
+        },
+        {
+            name: 'My journey to UX design',
+            desc: 'I think the world knows by now, that a design degree doesn’t make a good designer. Especially in Georgia, where UX design is not even…',
+            imgUrl: 'Journey.png'
+        }
+    ];
+
     return (
         <div>
-            <h1>Articles</h1>
+            {articles.map(x => (<p>{x.name}</p>))}
         </div>
     )
 }
+
+export default Articles;
--- pages/work.js
+++ pages/work.js
@@ -1,3 +1,37 @@
-export default function Work(){
-    return <h1>Work</h1>
+const Work = () => {
+
+    const works = [
+        {
+            name: 'TBC Payments',
+            desc: 'TBC Bank is one of the leading banks in Georgia, currently I’m responsible for ensuring smooth experience for merchants and creating the platform for digitalizing all their analytical needs. I am working with an agile team on improving payment experiences, as well as with the marketing team on redesigning related products and platforms.',
+            status: 'Currently working at',
+            imgUrl: 'tbc.png'
+        },
+        {
+            name: 'Adjarabet.com',
+            desc: 'I was developing Wireframes and prototypes around customer and business needs, working closely with marketing team to ensure right and suitable visuals and specifications for Promo pages, communicating designs to development team and working with them to produce competitive and engaging products for Adjarabet.com users. Working in a promotional team involved creating Ux flows, UI and graphic design assets and also currently used design system.',
+            status: 'Work done for',
+            imgUrl: 'adjara.png'
+        },
+        {
+            name: 'Individ fashion website & app',
+            desc: 'I was developing Wireframes and prototypes around customer and business needs, working closely with marketing team to ensure right and suitable visuals and specifications for Promo pages, communicating designs to development team and working with them to produce competitive and engaging products for Adjarabet.com users. Working in a promotional team involved creating Ux flows, UI and graphic design assets and also currently used design system.',
+            status: 'Work done for',
+            imgUrl: 'individ.png'
+        },
+        {
+            name: 'Jobspace',
+            desc: 'I was developing Wireframes and prototypes around customer and business needs, working closely with marketing team to ensure right and suitable visuals and specifications for Promo pages, communicating designs to development team and working with them to produce competitive and engaging products for Adjarabet.com users. Working in a promotional team involved creating Ux flows, UI and graphic design assets and also currently used design system.',
+            status: 'Work done for',
+            imgUrl: 'jspace.png'
+        }
+    ];
+
+    return (
+        <div>
+            {works.map(x => (<p>{x.name}</p>))}
+        </div>
+    )
 }
+
+export default Work;
