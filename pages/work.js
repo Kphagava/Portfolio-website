7102a892cc118a2599e619dd47b44d27219d7554
Lasha Loladze <l.loladze11@gmail.com>
2021-02-28 11:37:28 +0400

    Added links to work page

--- pages/work.js
+++ pages/work.js
@@ -1,4 +1,5 @@
 import styles from '../styles/work-cert.module.css'
+import Link from 'next/link'
 
 const Work = () => {
 
@@ -8,14 +9,17 @@ const Work = () => {
             name: 'TBC Payments',
             desc: 'TBC Bank is one of the leading banks in Georgia, currently I’m responsible for ensuring smooth experience for merchants and creating the platform for digitalizing all their analytical needs. I am working with an agile team on improving payment experiences, as well as with the marketing team on redesigning related products and platforms.',
             status: 'Currently working at',
-            imgUrl: 'tbc.png'
+            imgUrl: 'tbc.png',
+            websiteUrl: 'https://google.com'
         },
         {
             id: 1,
             name: 'Adjarabet.com',
             desc: 'I was developing Wireframes and prototypes around customer and business needs, working closely with marketing team to ensure right and suitable visuals and specifications for Promo pages, communicating designs to development team and working with them to produce competitive and engaging products for Adjarabet.com users. Working in a promotional team involved creating Ux flows, UI and graphic design assets and also currently used design system.',
             status: 'Work done for',
-            imgUrl: 'adjara.png'
+            imgUrl: 'adjara.png',
+            websiteUrl: 'https://google.com',
+            articleUrl: 'https://google.com'
         },
         {
             id: 2,
@@ -44,14 +48,31 @@ const Work = () => {
                             <div className={styles.itemHeader}>{item.name}</div>
                             <div className={styles.itemTxt}>{item.desc}</div>
                             <div className={styles.itemActions}>
-                                {/* <a href="" class="action-to-website action">
-                                    Visit Website
-                                    <img src="./assets/arrow-link.svg" alt="">
-                                </a>
-                                <a href="" class="action-to-article action">
-                                    Read Article 
-                                    <img src="./assets/arrow-link.svg" alt="">
-                                </a> */}
+                                {
+                                    item.websiteUrl !== undefined ?
+                                    <a 
+                                        target="_blank" 
+                                        href={item.websiteUrl} 
+                                        className={[styles.actionToWebsite, styles.action].join(' ')}
+                                        >
+                                        Visit Website
+                                        <img src="icons/arrow-link.svg" alt=""/>
+                                    </a>
+                                    : ''
+                                }
+
+                                {
+                                    item.articleUrl !== undefined ?
+                                    <a 
+                                        href={item.articleUrl}
+                                        target="_blank"
+                                        className={[styles.actionToArticle, styles.action].join(' ')}
+                                        >
+                                        Read Article 
+                                        <img src="icons/arrow-link.svg" alt=""/>
+                                    </a>
+                                    : ''
+                                }
                             </div>
                         </div>
                     </div>
